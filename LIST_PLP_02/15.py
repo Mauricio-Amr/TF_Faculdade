4e6aabcaa5f279b189b9f6f22b2416a2f587e4e2
Mauricio-Amr <mauricio.s.amr@gmail.com>
2021-10-10 01:41:30 -0300

    ex 15

---
+++ LIST_PLP_02/15.py
@@ -0,0 +1,13 @@
+''' -----------------------------------------------------------------
+    UNIVERSIDADE PAULISTA - ENG COMPUTAÇÃO - BACELAR
+    Nome: Mauricio Silva Amaral
+    RA : D92EJG-0
+    Turma : CP6P01
+    Data: 09/10/2021
+    Lista: 02 - 12
+    Enunciado: 15 – Escreva um programa para calcular a redução do tempo
+    de vida de um fumante. Pergunte a quantidade de cigarros fumados por
+    dia e quantos anos ela já fumou. Considere que um fumante perde 10
+    minutos de vida a cada cigarro e calcule quantos dias de vida um
+    fumante perderá. Exiba o total em dias.
+    ----------------------------------------------------------------- '''
