011f8969da852174a1b019fb0d44b9fdbfa1d701
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2021-10-10 22:50:17 -0300

    tf 4 -ex13 concluido

--- LIST_PLP_04/13.py
+++ LIST_PLP_04/13.py
@@ -10,41 +10,52 @@
     de juros pago.
     ----------------------------------------------------------------- '''
 
-
 # quanto tempo para paga a divida, inicializar contador
 meses = 0
-#valorJuros = 0
-
+totalJuros = 0
+totalPago = 0
 
 valorDivida = float(input("Digite o valor da divida : "))
 valorMensal = float(input("Digite o valor mensal para quitar a divida : "))
 jurosMensal = float(input("Digite o Juros : "))
 
-print(f'{valorMensal} = {jurosMensal:.2f} =  {valorDivida:.2f}')
+print('\n')
+print('Proposta')
+print(f' Valor da divida : {valorDivida:.2f}\n Valor do Juros : {jurosMensal:.2f}%\n Valor Mensal : {valorMensal:.2f}')
+print('*' * 50)
 
-# Juros mensal
-if valorMensal > valorDivida*(jurosMensal/100):
+# juros não pode ser maior que o valor mensal
+if valorMensal > valorDivida * (jurosMensal / 100):
     while valorDivida >= 0:
 
         valorJuros = valorDivida * (jurosMensal / 100)
         valorDivida = (valorDivida - valorMensal) + valorJuros
         meses += 1
-        print(f'{meses} = {valorJuros:.2f} =  {valorDivida:.2f}')
+        totalJuros += valorJuros
+        totalPago += valorMensal
+
+        print(
+            f'#Parcelas = {meses}'
+            f'\t#Juros = {valorJuros:.2f}'
+            f'\tTotal pago = {totalPago:.2f}'
+            f'\t#valor residual = {valorDivida:.2f}')
 
         if valorDivida < valorMensal:
             meses += 1
             valorJuros = 0
             valorMensal = valorDivida
             valorDivida = valorDivida - valorMensal
-            print(f'{meses} = {valorJuros:.2f} =  {valorDivida:.2f}')
+            totalPago += valorMensal
+            print(f'#Parcelas = {meses}'
+                  f'\t#Juros = {valorJuros:.2f}'
+                  f'\tTotal pago = {totalPago:.2f}'
+                  f'\t#valor residual = {valorDivida:.2f}')
+            print('*' * 50)
+
+            # total pago
+            print(f'Total de juros : {totalJuros:.2f}')
             break
 
 
 else:
     print(f'Proposta não atende')
-
-
-# total pago
-
-#juros não pode ser maior que o valor mensal
-
